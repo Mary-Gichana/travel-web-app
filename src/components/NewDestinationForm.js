4b3423859d4a70b9161ab89cccbe392c336e5e67
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 12:00:37 +0300

    set the value and onChangefor input fields to update state

--- src/components/NewDestinationForm.js
+++ src/components/NewDestinationForm.js
@@ -16,9 +16,27 @@ function NewDestinationForm({ handleAddDestination }) {
     <div className="new-destination-form">
       <h2>New Destination</h2>
       <form>
-        <input type="text" name="name" placeholder="Destination name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="text" name="description" placeholder="description" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Destination name"
+          value={newDestination.name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={newDestination.image}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          value={newDestination.description}
+          onChange={handleInputChange}
+        />
       </form>
     </div>
   );
