b14872b7f2c1e8f5bb19b1b4d1fdf2be4f3a800b
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 10:15:51 +0300

    Maps over data and renders each destination with DestinationCard component

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -3,13 +3,25 @@ import DestinationCard from "./DestinationCard";
 
 function Destinations() {
   const [destinations, setDestinations] = useState([]);
-  return useEffect(
+  useEffect(
     () =>
       fetch("http://localhost:3000/destinations")
         .then((response) => response.json())
         .then((data) => setDestinations(data))
         .catch((error) => console.log(error)),
     []
+  );
+  return (
+    <ul className="cards">
+      {destinations.map((destination) => (
+        <DestinationCard
+          key={destination.id}
+          name={destination.name}
+          image={destination.image}
+          description={destination.description}
+        />
+      ))}
+    </ul>
   );
 }
 
