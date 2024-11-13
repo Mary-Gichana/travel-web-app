6eedd820725121066361d46f6bfb1a13e40c9e5f
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 13:28:18 +0300

    restructured Destinations component by removing useState and useEffect

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,33 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DestinationCard from "./DestinationCard";
-import NewDestinationForm from "./NewDestinationForm";
 
-function Destinations() {
-  const [destinations, setDestinations] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/destinations")
-      .then((response) => response.json())
-      .then((data) => setDestinations(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  function handleAddDestination(newDestination) {
-    setDestinations([...destinations, newDestination]);
-  }
+function Destinations({ destinations }) {
   return (
-    <>
-      <ul className="cards">
-        {destinations.map((destination) => (
-          <DestinationCard
-            key={destination.id}
-            name={destination.name}
-            image={destination.image}
-            description={destination.description}
-          />
-        ))}
-      </ul>
-      <NewDestinationForm handleAddDestination={handleAddDestination} />
-    </>
+    <ul className="cards">
+      {destinations.map((destination) => (
+        <DestinationCard
+          key={destination.id}
+          name={destination.name}
+          image={destination.image}
+          description={destination.description}
+        />
+      ))}
+    </ul>
   );
 }
 
