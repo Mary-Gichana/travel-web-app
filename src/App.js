4eabe2a9ce47081a442f2a50ea2a4d7d18399e7e
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 13:29:18 +0300

    restructured App component by adding useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
+import NewDestinationForm from "./components/NewDestinationForm";
 import Destinations from "./components/Destinations";
+import Home from "./components/Home";
 
 function App() {
+  const [destinations, setDestinations] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/destinations")
+      .then((response) => response.json())
+      .then((data) => setDestinations(data))
+      .catch((error) => console.log(error));
+  }, []);
+  function handleAddDestination(newDestination) {
+    setDestinations([...destinations, newDestination]);
+  }
   return (
     <div className="App">
-      <Header />
       <Home />
-      <Destinations />
+      <Destinations destinations={destinations} />
+      <NewDestinationForm handleAddDestination={handleAddDestination} />
     </div>
   );
 }
