eaa4e661979b0a4c3cbf1122676daf6f7eadef81
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 11:29:40 +0300

    Add handleAddDestination function to add new Destination to the list

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from "react";
 import DestinationCard from "./DestinationCard";
+import NewDestinationForm from "./NewDestinationForm";
 
 function Destinations() {
   const [destinations, setDestinations] = useState([]);
-  useEffect(
-    () =>
-      fetch("http://localhost:3000/destinations")
-        .then((response) => response.json())
-        .then((data) => setDestinations(data))
-        .catch((error) => console.log(error)),
-    []
-  );
+  useEffect(() => {
+    fetch("http://localhost:3000/destinations")
+      .then((response) => response.json())
+      .then((data) => setDestinations(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  function handleAddDestination(newDestination) {
+    setDestinations([...destinations, newDestination]);
+  }
   return (
-    <ul className="cards">
-      {destinations.map((destination) => (
-        <DestinationCard
-          key={destination.id}
-          name={destination.name}
-          image={destination.image}
-          description={destination.description}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className="cards">
+        {destinations.map((destination) => (
+          <DestinationCard
+            key={destination.id}
+            name={destination.name}
+            image={destination.image}
+            description={destination.description}
+          />
+        ))}
+      </ul>
+      <NewDestinationForm handleAddDestination={handleAddDestination} />
+    </>
   );
 }
 
