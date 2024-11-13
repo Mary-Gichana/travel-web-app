9f106ac27d93244a6f84e9e73dcd820d307358a4
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 10:39:05 +0300

    added Card content

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 
-function DestinationCard() {
+function DestinationCard({ name, image, description }) {
   const [visited, setVisited] = useState(true);
 
-  return <li></li>;
+  return (
+    <li className="card">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>{description}</p>
+      <button className="btn-1"></button>
+    </li>
+  );
 }
 
 export default DestinationCard;
