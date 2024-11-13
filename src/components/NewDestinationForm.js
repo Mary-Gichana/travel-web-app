914250076be641f2d25bfa066a751ace364fdc9b
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 11:54:45 +0300

    Added handleInputChange function to update form state for new destination

--- src/components/NewDestinationForm.js
+++ src/components/NewDestinationForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Destinations from "./Destinations";
 
 function NewDestinationForm({ handleAddDestination }) {
   const [newDestination, setnewDestination] = useState({
@@ -6,14 +7,18 @@ function NewDestinationForm({ handleAddDestination }) {
     image: "",
     description: "",
   });
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setnewDestination({ ...newDestination, [name]: value });
+  }
 
   return (
     <div className="new-destination-form">
       <h2>New Destination</h2>
       <form>
-        <input />
-        <input />
-        <input />
+        <input type="text" name="name" placeholder="Destination name" />
+        <input type="text" name="image" placeholder="Image URL" />
+        <input type="text" name="description" placeholder="description" />
       </form>
     </div>
   );
