5c64acda622d655ce6c91f6588845b5cf996d29e
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-13 12:26:27 +0300

    Added Handle server response and update destinations list

--- src/components/NewDestinationForm.js
+++ src/components/NewDestinationForm.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
-import Destinations from "./Destinations";
 
 function NewDestinationForm({ handleAddDestination }) {
-  const [newDestination, setnewDestination] = useState({
+  const [newDestinationData, setnewDestinationData] = useState({
     name: "",
     image: "",
     description: "",
   });
   function handleInputChange(e) {
     const { name, value } = e.target;
-    setnewDestination({ ...newDestination, [name]: value });
+    setnewDestinationData({ ...newDestinationData, [name]: value });
   }
   function handleFormSubmit(e) {
     e.preventDefault();
     fetch("http://localhost:3000/destinations", {
       method: "POST",
       headers: { "Content-Type": "Application/JSON" },
-      body: JSON.stringify(newDestination),
-    });
+      body: JSON.stringify(newDestinationData),
+    })
+      .then((response) => response.json())
+      .then((newDestination) => {
+        handleAddDestination(newDestination);
+        setnewDestinationData({
+          name: "",
+          image: "",
+          description: "",
+        });
+      });
   }
 
   return (
@@ -28,21 +36,21 @@ function NewDestinationForm({ handleAddDestination }) {
           type="text"
           name="name"
           placeholder="Destination name"
-          value={newDestination.name}
+          value={newDestinationData.name}
           onChange={handleInputChange}
         />
         <input
           type="text"
           name="image"
           placeholder="Image URL"
-          value={newDestination.image}
+          value={newDestinationData.image}
           onChange={handleInputChange}
         />
         <input
           type="text"
           name="description"
           placeholder="description"
-          value={newDestination.description}
+          value={newDestinationData.description}
           onChange={handleInputChange}
         />
       </form>
