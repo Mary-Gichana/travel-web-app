934b808319e8c4af4f77327fceba266be63a6ced
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-14 17:36:20 +0300

    added logo content

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,9 @@ function Home() {
         <h1>Welcome To The Travel App</h1>
         <p>
           Explore beautiful destinations around the world. Start your journey
-          today!.With this App you can be able to view and keep track of the
-          destinations that you have visited and those you have not visited,You
-          can delete a destination from the destination list and also add a new
-          destination.
+          today!.With this App you can be able to view and toggle destinations
+          that you have visited and those you have not visited,You can delete a
+          destination from the destination list and also add a new destination.
         </p>
       </main>
     </div>
