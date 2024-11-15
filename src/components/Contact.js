a4fdd0b075c93b6ef4c968a92ac8739d54c7cbcb
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-15 16:31:37 +0300

    added content to Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function contact() {
+  return (
+    <div>
+      Phone no :0758794309 <br />
+      email:marygichana95@gmail.com <br />
+      Address:Nairobi, Kenya
+    </div>
+  );
+}
+
+export default contact;
