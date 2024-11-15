8fd43971ba15bd2ac68394b701bbc0588d526c12
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-15 18:12:25 +0300

    changed h1 to h2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function contact() {
   return (
-    <div>
+    <div className="contact">
+      <h2>Contact Us</h2>
       Phone no :0758794309 <br />
       email:marygichana95@gmail.com <br />
       Address:Nairobi, Kenya
