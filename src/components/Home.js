9ae768dc4d8cdd95bcdd8c285cd6fff9fe0f7aa3
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-15 15:46:18 +0300

    added image cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,36 @@ function Home() {
           that you have visited and those you have not visited,You can delete a
           destination from the destination list and also add a new destination.
         </p>
+        <div className="img-2">
+          <div className="imgcard">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/05/08/15/11/capetown-2295679_1280.jpg"
+              alt="image-4"
+            />
+            <p>beautiful oceans</p>
+          </div>
+          <div className="imgcard">
+            <img
+              src="https://cdn.pixabay.com/photo/2024/09/13/17/05/beach-9045462_1280.jpg"
+              alt="image-5"
+            />
+            <p>beautiful scenery</p>
+          </div>
+          <div className="imgcard">
+            <img
+              src="https://cdn.pixabay.com/photo/2022/06/06/21/58/bird-7247045_1280.jpg"
+              alt="image-6"
+            />
+            <p>beautiful birds</p>
+          </div>
+          <div className="imgcard">
+            <img
+              src="https://cdn.pixabay.com/photo/2019/09/29/20/29/capetown-4514288_1280.jpg"
+              alt="image-7"
+            />
+            <p>Awesome animals</p>
+          </div>
+        </div>
       </main>
     </div>
   );
