03c61c4fafab79b3b8325ac9e6aed218b473c6ca
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-29 13:34:38 +0300

    changed Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function contact() {
   return (
     <div className="contact">
       <h2>Contact Us</h2>
-      Phone no :0758794309 <br />
+      Phone :0758794309 <br />
       email:marygichana95@gmail.com <br />
       Address:Nairobi, Kenya
     </div>
